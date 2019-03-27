df54224a67166880dd591cfce2b6a57dc81f79ba
hacker1024 <joshleivenzon@outlook.com>
2019-03-27 20:35:31 +1100

    beryllium: Build PocketMode app
    Change-Id: Ia7bde817fbd4ac854ef8b9a6d54e9e597487ee5a

--- device.mk
+++ device.mk
@@ -57,5 +57,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    XiaomiPocketMode
+
 # Build Official
 export export HAVOC_BUILD_TYPE=Official
