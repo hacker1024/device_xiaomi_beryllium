ee34837493e3cd6fbbf9762e59a40d38fb5f44dc
hacker1024 <joshleivenzon@outlook.com>
2019-03-27 20:35:31 +1100

    Revert "beryllium: Build PocketMode app"
    This reverts commit df54224a67166880dd591cfce2b6a57dc81f79ba.

--- device.mk
+++ device.mk
@@ -57,9 +57,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 # Build Official
 export export HAVOC_BUILD_TYPE=Official
